4e0c08ea22e32f70d2b8adbd733c389567a6c05a
Trịnh Huỳnh Thịnh Khang <107709392+Tkag0001@users.noreply.github.com>
2024-12-11 21:57:53 +0700

    update: folder for testing

---
+++ deploy_streamlit/app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.set_page_config(layout="wide")
+
+st.header("Trịnh Huỳnh Thịnh Khang 🌻")
+st.subheader("Cảm ơn mọi người đã ghé qua và thăm repository của mình nha <3")
+st.image("./my_crush.png")
